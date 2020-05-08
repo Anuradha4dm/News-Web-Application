d0d92044bd2184c271bd0303af77c6a6a67be995
Anuradha4dm <damithanuradha44@gmail.com>
2020-05-08 22:05:09 +0530

    Initial structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-application';
+  
+  selectedElement='recipes';
+
+  onNavigation(event){
+
+    this.selectedElement=event;
+  }
+
+ 
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LatestnewsComponent } from './components/latestnews/latestnews.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { AllnewsComponent } from './components/allnews/allnews.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NewsdetailsComponent } from './components/latestnews/newsdetails/newsdetails.component';
+import { NewslistComponent } from './components/latestnews/newslist/newslist.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LatestnewsComponent, NavigationComponent, AllnewsComponent, FooterComponent, NewsdetailsComponent, NewslistComponent],
+  imports: [BrowserModule, FormsModule, CommonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
